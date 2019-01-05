df9f697d862d2004a248a482f58e3eb476e0193b
Coraline Ada Ehmke <coraline@idolhands.com>
2019-01-04 21:28:04 -0600

    Integrate Recaptcha

---
+++ app/controllers/accounts/passwords_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class Accounts::PasswordsController < Devise::PasswordsController
+
+  prepend_before_action :check_captcha, only: [:create]
+
+  # GET /resource/password/new
+  # def new
+  #   super
+  # end
+
+  # POST /resource/password
+  # def create
+  #   super
+  # end
+
+  # GET /resource/password/edit?reset_password_token=abcdef
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource/password
+  # def update
+  #   super
+  # end
+
+  # protected
+
+  # def after_resetting_password_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sending reset password instructions
+  # def after_sending_reset_password_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
+
+  private
+
+  def check_captcha
+    unless verify_recaptcha
+      self.resource = resource_class.new
+      resource.validate
+      respond_with_navigational(resource) { render :new }
+    end
+  end
+
+end
