c04c3272045c43b5e269044c81c7cb52bdff54a5
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2019-01-11 19:18:04 +0100

    Fix all offences to ClassAndModuleChildren rule

--- app/controllers/accounts/passwords_controller.rb
+++ app/controllers/accounts/passwords_controller.rb
@@ -1,46 +1,48 @@
 # frozen_string_literal: true
 
-class Accounts::PasswordsController < Devise::PasswordsController
-  prepend_before_action :check_captcha, only: [:create]
+module Accounts
+  class PasswordsController < Devise::PasswordsController
+    prepend_before_action :check_captcha, only: [:create]
 
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
+    # GET /resource/password/new
+    # def new
+    #   super
+    # end
 
-  # POST /resource/password
-  # def create
-  #   super
-  # end
+    # POST /resource/password
+    # def create
+    #   super
+    # end
 
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
+    # GET /resource/password/edit?reset_password_token=abcdef
+    # def edit
+    #   super
+    # end
 
-  # PUT /resource/password
-  # def update
-  #   super
-  # end
+    # PUT /resource/password
+    # def update
+    #   super
+    # end
 
-  # protected
+    # protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+    # def after_resetting_password_path_for(resource)
+    #   super(resource)
+    # end
 
-  # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+    # The path used after sending reset password instructions
+    # def after_sending_reset_password_instructions_path_for(resource_name)
+    #   super(resource_name)
+    # end
 
-  private
+    private
 
-  def check_captcha
-    return if verify_recaptcha
+    def check_captcha
+      return if verify_recaptcha
 
-    self.resource = resource_class.new
-    resource.validate
-    respond_with_navigational(resource) { render :new }
+      self.resource = resource_class.new
+      resource.validate
+      respond_with_navigational(resource) { render :new }
+    end
   end
 end
