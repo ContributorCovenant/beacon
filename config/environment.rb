47065a2bb303f7530b94b64c24c01e4cc314ae9e
Coraline Ada Ehmke <coraline@idolhands.com>
2019-01-15 19:04:19 -0600

    More mail config

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,7 @@ ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'coc-beacon.org',
+  :host => 'coc-beacon.herokuapp.com'
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
