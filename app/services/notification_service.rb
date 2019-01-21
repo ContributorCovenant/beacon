24dd4e9538647849d3c3181e1d2e6fecb592e58f
Coraline Ada Ehmke <coraline@idolhands.com>
2019-01-20 22:07:48 -0600

    Fix rubocop issue

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -1,6 +1,6 @@
 class NotificationService
 
-  def self.notify(project:, issue:, issue_comment:nil, account:)
+  def self.notify(project:, issue:, issue_comment: nil, account:)
     Notification.create(
       project_id: project.id,
       issue_id: issue.id,
