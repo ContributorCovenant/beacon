47e3fc074b86d62627aee405ec0f4ac18f8cc33e
Coraline Ada Ehmke <coraline@idolhands.com>
2019-02-09 19:07:20 -0800

    Clean up settings on project show

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -1,13 +1,11 @@
 class DirectoryController < ApplicationController
 
   def index
-    # TODO: this query is going to need to be optimized before performance becomes an issue
-    @projects = Project.includes(:project_setting).all.order(:name).select(&:show_in_directory?)
+    @projects = Project.directory
   end
 
   def show
-    @project = Project.find_by(slug: params[:slug])
-    redirect_to directory_path unless @project.show_in_directory?
+    redirect_to directory_path unless @project = Project.public.find_by(slug: params[:slug])
     @issue_severity_levels = @project.issue_severity_levels
   end
 end
