29bbef894d39648e19a7b558b4743a962acec23c
moduler <mq@mtomady.com>
2019-03-24 21:46:26 +0100

    dockerize resque & redis

---
+++ config/initializers/resque.rb
@@ -0,0 +1,6 @@
+rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
+rails_env = ENV['RAILS_ENV'] || 'development'
+config_file = rails_root + '/config/resque.yml'
+
+resque_config = YAML::load(ERB.new(IO.read(config_file)).result)
+Resque.redis = resque_config[rails_env]
