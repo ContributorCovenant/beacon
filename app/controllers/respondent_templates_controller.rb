30dce7e5ffa7dd0678ac3ada5888b9f5e6854596
Coraline Ada Ehmke <coraline@idolhands.com>
2019-03-27 19:49:02 -0500

    Fix URL in clone form

--- app/controllers/respondent_templates_controller.rb
+++ app/controllers/respondent_templates_controller.rb
@@ -5,16 +5,9 @@ class RespondentTemplatesController < ApplicationController
   before_action :scope_organization
   before_action :enforce_permissions
   before_action :scope_available_templates, except: [:update]
+  before_action :set_breadcrumbs
 
   def new
-    if organization = @organization || @project.organization
-      breadcrumb "Organizations", organizations_path
-      breadcrumb organization.name, organization_path(organization)
-      breadcrumb(@project.name, project_path(@project)) if @project
-    else
-      breadcrumb "Projects", projects_path if @project
-      breadcrumb(@project.name, project_path(@project))
-    end
     if @project
       @template = RespondentTemplate.new(project_id: @project.id)
     else
@@ -25,7 +18,7 @@ class RespondentTemplatesController < ApplicationController
   def create
     if @project
       @template = RespondentTemplate.new(respondent_template_params.merge(project_id: @project.id))
-    elsif @organization
+    else
       @template = RespondentTemplate.new(respondent_template_params.merge(organization_id: @organization.id))
     end
     if @template.save
@@ -38,13 +31,6 @@ class RespondentTemplatesController < ApplicationController
   end
 
   def clone
-    if @organization
-      breadcrumb "Organizations", organizations_path
-      breadcrumb @organization.name, organization_path(@organization)
-    else
-      breadcrumb "Projects", projects_path
-    end
-    breadcrumb @project.name, project_path(@project)
     if existing = @subject.respondent_template
       existing.destroy
     end
@@ -52,7 +38,7 @@ class RespondentTemplatesController < ApplicationController
     if source == "Beacon Default"
       @template = @subject.create_respondent_template(text: RespondentTemplate.beacon_default.text)
     elsif source == "Organization Default"
-      @template = @subject.create_respondent_template(text: @organization&.respondent_template&.text || @project.organization&.respondent_template.text)
+      @template = @subject.create_respondent_template(text: @organization.respondent_template.text || @project.organization.respondent_template.text)
     else
       source = current_account.projects.find_by(name: source).respondent_template
       @template = @subject.create_respondent_template(text: source.text)
@@ -62,14 +48,6 @@ class RespondentTemplatesController < ApplicationController
   end
 
   def edit
-    if organization = @organization || @project.organization
-      breadcrumb "Organizations", organizations_path
-      breadcrumb organization.name, organization_path(organization)
-      breadcrumb(@project.name, project_path(@project)) if @project
-    else
-      breadcrumb "Projects", projects_path if @project
-      breadcrumb(@project.name, project_path(@project))
-    end
     @template = @subject.respondent_template
   end
 
@@ -94,29 +72,41 @@ class RespondentTemplatesController < ApplicationController
   end
 
   def scope_available_templates
-    if @project&.organization
-      projects_with_respondent_template = @project.organization.projects.select(&:respondent_template?).map(&:name)
-    elsif @organization
+    if @organization
       projects_with_respondent_template = @organization.projects.select(&:respondent_template?).map(&:name)
+    else
+      projects_with_respondent_template ||= current_account.personal_projects.select(&:respondent_template?).map(&:name)
     end
-    projects_with_respondent_template ||= current_account.personal_projects.select(&:respondent_template?).map(&:name)
     if @project
       org_template = @project.organization.present? && @project.organization.respondent_template? ? "Organization Default" : nil
-    else
+    elsif @organization.respondent_template?
       org_template = "Organization Default"
+    else
+      org_template = nil
     end
     @available_templates = ["Beacon Default", org_template, projects_with_respondent_template].flatten.compact - [@project&.name]
   end
 
   def scope_organization
-    @organization = Organization.find_by(slug: params[:organization_slug])
-    @organization ||= @project.organization
+    @organization ||= Organization.find_by(slug: params[:organization_slug])
     @subject ||= @organization
   end
 
   def scope_project
-    @project = Project.find_by(slug: params[:project_slug])
+    return unless @project = Project.find_by(slug: params[:project_slug])
+    @organization = @project.organization
     @subject = @project
+  end
+
+  def set_breadcrumbs
+    if organization = @organization || @project.organization
+      breadcrumb "Organizations", organizations_path
+      breadcrumb organization.name, organization_path(organization)
+      breadcrumb(@project.name, project_path(@project)) if @project
+    else
+      breadcrumb "Projects", projects_path if @project
+      breadcrumb(@project.name, project_path(@project))
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -182,7 +182,6 @@ ActiveRecord::Schema.define(version: 2019_03_27_010650) do
     t.string "uid"
     t.string "email"
     t.uuid "account_id"
-    t.string "oauth_token"
     t.string "token_encrypted"
     t.index ["account_id"], name: "index_credentials_on_account_id"
     t.index ["provider", "uid"], name: "index_credentials_on_provider_and_uid", unique: true
@@ -267,14 +266,11 @@ ActiveRecord::Schema.define(version: 2019_03_27_010650) do
     t.string "slug"
     t.text "description"
     t.uuid "account_id"
-    t.datetime "flagged_at"
-    t.text "flagged_reason"
-    t.datetime "confirmed_at"
-    t.string "confirmation_token_url"
     t.string "remote_org_name"
-    t.datetime "created_at", default: "2019-03-16 00:00:00"
-    t.datetime "updated_at", default: "2019-03-16 00:00:00"
+    t.datetime "created_at", default: "2019-03-27 00:00:00"
+    t.datetime "updated_at", default: "2019-03-27 00:00:00"
     t.boolean "is_flagged", default: false
+    t.text "flagged_reason"
     t.index ["account_id"], name: "index_organizations_on_account_id"
   end
 
@@ -319,7 +315,6 @@ ActiveRecord::Schema.define(version: 2019_03_27_010650) do
     t.uuid "organization_id"
     t.string "confirmation_token_url"
     t.string "repo_url"
-    t.datetime "start_date"
     t.boolean "is_event", default: false
     t.integer "duration"
     t.string "frequency"
