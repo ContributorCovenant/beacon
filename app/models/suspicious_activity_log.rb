bbc283a41d0c5907c685e0df2bc67ee3cf65c7eb
Coraline Ada Ehmke <coraline@idolhands.com>
2019-05-05 17:29:45 -0500

    ENV variables to_i

--- app/models/suspicious_activity_log.rb
+++ app/models/suspicious_activity_log.rb
@@ -2,6 +2,6 @@ class SuspiciousActivityLog < ApplicationRecord
 
   def self.throttle(remote_ip)
     return unless where(ip_address: remote_ip, created_at: (DateTime.now - 5.minutes)..DateTime.now).count >= 10
-    Rails.cache.write("block #{remote_ip}", true, expires_in: ENV.fetch('THROTTLE_MINUTES', 10).minutes)
+    Rails.cache.write("block #{remote_ip}", true, expires_in: ENV.fetch('THROTTLE_MINUTES', 10).to_i.minutes)
   end
 end
