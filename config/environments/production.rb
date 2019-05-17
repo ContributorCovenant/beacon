75bbde5211390f2d01f31292425d824fb318518e
Coraline Ada Ehmke <coraline@idolhands.com>
2019-05-17 17:03:59 -0500

    Use redis for production cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
 
   config.action_mailer.default_url_options = { host: 'www.coc-beacon.com' }
 
